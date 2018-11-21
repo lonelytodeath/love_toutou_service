8508e62d1d508ed1aa5f26f63c126f2740ff88ea
weichao <18410400037@163.com>
2018-11-21 21:45:51 +0800

    add config.pro

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,7 +4,7 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg';
+    this.ctx.body = '头头和皱皱是天生一对，他们有一个狗达和一个丫鬟，狗达叫嗡嗡怪，丫鬟叫小毛';
   }
 }
 
---
+++ config/config.prod.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = appInfo => {
+  const config = exports = {};
+  config.keys = appInfo.name + '_1532962019674_2847';
+  config.cluster = {
+    listen: {
+      port: 10000,
+      hostname: '0.0.0.0',
+    }
+  }; 
+  return config;
+}
